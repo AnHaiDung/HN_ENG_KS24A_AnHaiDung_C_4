7e92b2b6b5a1d0538a7f595401bbd3ee9ac9c02d
AnHaiDung <anhaidung2k6@gmail.com>
2024-11-24 19:37:31 +0700

    b2

---
+++ Ss4-b2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+ int main(){
+ 	int number;
+ 	printf("nhap so nguyen");
+ 	scanf("%d",&number);
+ 	if (number %2 == 0){
+ 		printf("day la so chan");
+	 }else{
+	 	printf("day la so le");
+	 }
+return 0;
+ }

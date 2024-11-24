2126a05dee30f1d00ec1106ae9a0a6ab67836a3a
AnHaiDung <anhaidung2k6@gmail.com>
2024-11-24 19:37:46 +0700

    b3

---
+++ Ss4-b3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+	int number;
+	printf("nhap 1 so nguyen");
+	scanf("%d", &number);
+	if (number%3==0){
+		printf("chia het cho 3");
+	}else if ( number%5==0){
+		printf("chia het cho 5");
+	}else if (number%3==0&&number%5==0){
+		printf("chia het cho ca 3 va 5");
+	}else{
+		printf("khong chia het cho ca 3 va 5");
+	}
+	return 0;
+}
